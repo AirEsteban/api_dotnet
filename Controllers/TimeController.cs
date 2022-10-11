e6d523a851c5d22dc81d2a11a6817b51a9baa907
AirEsteban <airascaesteban@gmail.com>
2022-10-11 19:29:42 -0300

    Dependency Injection was added for a little datetime controller

---
+++ Controllers/TimeController.cs
@@ -0,0 +1,21 @@
+﻿using Api_dotnet.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_dotnet.Controllers;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeController : ControllerBase
+    {
+        public ITimeService _timeService;
+        public TimeController(ITimeService time)
+        {
+            this._timeService = time;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(this._timeService.GetDateTime());
+        }
+    }
---
+++ Services/TimeService.cs
@@ -0,0 +1,15 @@
+﻿namespace Api_dotnet.Services
+{
+    public class TimeService : ITimeService
+    {
+        public string GetDateTime()
+        {
+            return DateTime.Now.ToLocalTime().ToString();
+        }
+    }
+
+    public interface ITimeService
+    {
+        string GetDateTime();
+    }
+}
