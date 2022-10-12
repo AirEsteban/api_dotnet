81dd4b265f5b1d880e324bdf34c7cdffb33b0c1e
AirEsteban <airascaesteban@gmail.com>
2022-10-12 17:35:32 -0300

    Log added when the get endpoint of the time controller is requested

--- Controllers/TimeController.cs
+++ Controllers/TimeController.cs
@@ -7,15 +7,18 @@ namespace Api_dotnet.Controllers;
     [Route("[controller]")]
     public class TimeController : ControllerBase
     {
+        private readonly ILogger<TimeController> _logger;
         public ITimeService _timeService;
-        public TimeController(ITimeService time)
+        public TimeController(ITimeService time, ILogger<TimeController> log)
         {
             this._timeService = time;
+            this._logger = log;
         }
 
         [HttpGet]
         public IActionResult Get()
         {
+            this._logger.LogDebug("Getting the local datetime.");
             return Ok(this._timeService.GetDateTime());
         }
     }
