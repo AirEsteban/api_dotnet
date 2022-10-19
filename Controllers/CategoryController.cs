2507f8e7dd0ca2de88452a695c2984c9e2fb19af
AirEsteban <airascaesteban@gmail.com>
2022-10-18 21:16:44 -0300

    Adding the category controller to use category service

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Api_dotnet.Services;
+using Microsoft.AspNetCore.Mvc;
+using webapi;
+using webapi.Models;
+
+namespace Api_dotnet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private CategoryService categoryService;
+
+        public CategoryController(CategoryService service)
+        {
+            this.categoryService = service;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return categoryService.GetAllCategories();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult GetAlgo()
+        {
+            return Ok("Algo bien.");
+        }
+
+    }
+}
